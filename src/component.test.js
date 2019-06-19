8725578c338a28d6299e4e427c6d1718819fd6a0
Fatih Telis <fatih.telis@fabrikamedya.com.tr>
2019-06-19 16:06:16 +0300

    Test file import route fix

--- src/component.test.js
+++ src/component.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Component from '..';
+import Component from '.';
 
 Enzyme.configure({ adapter: new Adapter() });
 
